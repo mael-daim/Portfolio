859fd05e7a1d9451eb801856928e93169e325317
mael.daim <Mael.DAIM@etu.uca.fr>
2025-11-07 20:18:33 +0100

    updating and adding localization

---
+++ src/localization/strings.js
@@ -0,0 +1,155 @@
+const translations = {
+  en: {
+
+    cvPath: "src/imgs/CV_Mael-daim_English.pdf",
+    commonClickHere : "Click here",
+
+    homeText : "Home",
+    aboutTitle : "About Me",
+    myWorkTitle : "My Projects",
+    interestsTitle : "My Interests",
+    contactTitle : "Contact Me",
+    greeting: "Hello !",
+    iAm: "I'm Maël Daim",
+    status: "Student – Full-Stack Developer",
+
+    who : "Who am I?",
+    presentation : `As a first-year Master's student in Computer Science, I discovered development out of curiosity, wanting to understand how video games are made.<br/>
+            I now aspire to become a <strong>full-stack developer with a focus on mobile applications</strong> after completing my studies.<br/>
+            I’m determined to strengthen my skills and gain valuable experience.`,
+
+    cvLink : "View my CV",
+
+    skills : "My Skills",
+    programmingLanguages : "Programming Languages",
+    plList : "HTML - CSS - Swift - Java - Python - Kotlin - PHP",
+    frameworks : "Frameworks",
+    fList : "Spring - Jetpack Compose - SwiftUI",
+    knwoledge : "Knowledge",
+    kList : "Git - PostgreSQL - REST APIs - SOLID principles",
+    softSkills : "Soft Skills",
+    sList : "Autonomy - Adaptability - Intellectual Curiosity",
+
+    project1Title : "IQBall",
+    project1Description : `Developed a web platform to replace basketball coaches’ playbooks, along with a mobile app for viewing and sharing tactics. 
+                                            This was a team project of five, built using React, PHP, C#, and Jetpack Compose, following the SCRUM methodology.<br>
+                                            In short:<br>
+                                            - Application development<br>
+                                            - Database management<br>
+                                            - Project coordination and teamwork<br><br/>
+                                            09/2023 – 03/2024`,
+
+    project2Title : "Portfolio",
+    project2Description : `Designed and developed my personal portfolio using HTML, CSS, and JavaScript.<br/><br/>
+                                            03/2024 – 07/2025`,
+
+    project3Title : "Virtogen",
+    project3Description : `An educational genetics simulation software developed to replace an outdated UCA teaching tool.<br/>
+                                            Designed for biology students and teachers, it enables virtual manipulation of fruit fly populations.
+                                            We built a complete backend with genetic logic and a minimalist frontend, in collaboration with the DOSI, ensuring long-term maintainability.<br/><br/>
+                                            10/2024 – 12/2024`,
+
+    interest1 : "Philosophy",
+    interest1Description : "Philosophy offers me new ways of understanding the world and sharpens my critical thinking.",
+    interest2 : "Video Games",
+    interest2Description : "Video games nurture my competitive spirit and perseverance.",
+    interest3 : "Sports",
+    intesrest3Description : "Basketball and sports in general, teach me teamwork, discipline, and determination.",
+
+    digitalSpaceText : `In my free time, I’m developing a <a href="https://www.notion.so/Espace-personnel-21e52d7d148b80b99e96fed38f3db511?source=copy_link" target="_blank"><strong><u>personal digital space</u></strong></a> that gathers notes, reflections, and readings I build upon over time. 
+                    There, I explore a wide range of topics, particularly philosophical works, as well as political and cultural themes. 
+                    This space mainly reflects the approach of a curious beginner, eager to understand, articulate ideas, and deepen his knowledge. 
+                    I chose to make it accessible from my portfolio because it complements my overall approach: it demonstrates a desire for personal growth, critical analysis, and intellectual rigor that I aim to bring into my professional projects.`,
+
+    joinMe : "Where to Reach Me?",
+    contact1: "LinkedIn",
+    contact2: "Email",
+  },
+
+  fr: {
+
+    commonClickHere : "Cliquez ici",
+
+    homeText : "Accueil",
+    aboutTitle : "À propos de moi",
+    myWorkTitle : "Mes réalisations",
+    interestsTitle : "Mes centres d'intérêts",
+    contactTitle : "Me contacter",
+    greeting: "Bonjour !",
+    iAm: "Je suis Maël Daim",
+    status: "Étudiant - développeur full-stack",
+
+    who : "Qui suis-je ?",
+    presentation : `Étudiant en première année de master informatique, j'ai découvert le développement par curiosité afin de comprendre comment sont créés les jeux vidéo.<br/>
+        Je souhaite maintenant devenir <strong>développeur full-stack orienté mobile</strong> à la sortie de mes études.<br/>
+        Je suis déterminé à monter en compétences et à gagner en expérience.`,
+    cvLink : "Voir mon CV",
+    cvPath: "src/imgs/CV_Mael-daim.pdf",
+
+
+    skills : "Mes compétences",
+    programmingLanguages : "Langages de programmation",
+    plList : "Html - CSS - Swift - Java - Python - Kotlin - PHP ",
+    frameworks : "Frameworks",
+    fList : " Spring - Jetpack Compose - SwiftUI ",
+    knwoledge : "Connaissances",
+    kList : " Git - PostgreSQL - API REST - Concepts SOLID",
+    softSkills : "Compétences humaines",
+    sList : "Autonomie - Adaptabilité - Curiosité intellectuelle",
+
+    project1Title : "IQBall",
+    project1Description : `Réalisation d'un site web remplaçant la plaquette des coachs de basket et d'une application pour visualiser les tactiques créées et partagées. 
+                                        Ceci en groupe de 5 et en React/PHP/C#/Compose/... 
+                                        En organisation SCRUM<br>
+                                        Pour résumé :<br>
+                                        - Développement d'application<br>
+                                        - Gestion d'une base de données<br>
+                                        - Conduire un projet et travailler en équipe<br><br/>
+                                        09/2023 - 03/2024`,
+    project2Title : "Portfolio",
+    project2Description : `Mise en place en Html/CSS/Javascript de mon portfolio.<br/><br/>
+                                         03/2024 - 07/2025`,
+    project3Title : "Virtogen",
+    project3Description : `Logiciel pédagogique de simulation génétique développé pour un outil veillissant de l’UCA.<br/>
+                                        Pensé pour les étudiants et enseignants en biologie, il permet de manipuler virtuellement des populations de drosophiles.
+                                        Nous avons conçu un backend complet avec logique génétique et un front minimaliste, en collaboration avec la DOSI pour une reprise durable.<br/><br/>
+                                        10/2024 - 12/2024`,
+                                    
+    interest1 : "Philosophie",
+    interest1Description : "Néophyte de Schopenhauer, la philosophie m’apporte des grilles de lecture du monde et un esprit critique",
+    interest2 : "Jeux vidéo",
+    interest2Description : "Les jeux vidéo développent mon goût pour la compétition et ma persévérance. ",
+    interest3 : "Sports",
+    intesrest3Description : "Le basket et le sport en général m’inculquent l’esprit d’équipe, la discipline et la persévérance.",
+    digitalSpaceText :`Je développe sur mon temps libre un <a href="https://www.notion.so/Espace-personnel-21e52d7d148b80b99e96fed38f3db511?source=copy_link" target="_blank"><strong><u>espace numérique personnel</u></strong></a> qui accueille des notes, des réflexions et des lectures que je développe au fil du temps. 
+                J’y explore des sujets variés, en particulier des œuvres philosophiques ainsi que des thématiques politiques ou culturelles. 
+                Cet espace reflète avant tout une démarche de néophyte curieux, désireux de comprendre, de formuler des idées et de progresser dans son rapport au savoir. 
+                J’ai choisi de le rendre accessible depuis le portfolio car il complète ma démarche : il montre une volonté d’approfondissement personnel, d’analyse, et une certaine exigence intellectuelle que j’espère transposer dans mes projets professionnels.`,
+    
+    joinMe : "Où me joindre ?",
+    contact1: "Linkedin",
+    contact2: "Email",
+}
+};
+
+function setLanguage(lang) {
+  const elements = document.querySelectorAll("[id]");
+  elements.forEach(el => {
+    const key = el.id;
+    if (translations[lang][key]) {
+      el.innerHTML = translations[lang][key];
+    }
+  });
+
+  const cvLink = document.getElementById("cvLink");
+  if (cvLink && translations[lang].cvPath) {
+    cvLink.href = translations[lang].cvPath;
+  }
+
+  localStorage.setItem("lang", lang);
+}
+
+window.onload = () => {
+  const lang = localStorage.getItem("lang") || "en";
+  setLanguage(lang);
+};
